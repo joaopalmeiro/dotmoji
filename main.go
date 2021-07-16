beb526381fdebd5b04c650a46ddfb37b7b864153
João Palmeiro <joaommpalmeiro@gmail.com>
2021-07-16 21:13:19 +0100

    Update main.go

--- main.go
+++ main.go
@@ -15,4 +15,18 @@ func main() {
 
 	// fmt.Print(message)
 	fmt.Println(message)
+
+	// Printf (formatted strings).
+	// Add `\n` so that `%` does not appear
+	// at the end of the string on the command
+	// line (https://stackoverflow.com/a/22816721)
+
+	// `%v` (format specifier): Default format.
+	fmt.Printf("Message: %v\n", message)
+
+	// `%q`: Put quotes around the variable (for strings).
+	fmt.Printf("Message: %q\n", message)
+
+	// `%T`: Get the variable type.
+	fmt.Printf("Message: %T\n", message)
 }
