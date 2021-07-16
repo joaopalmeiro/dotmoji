b5c679084279ce20719e0d9b0058ce0840f41120
João Palmeiro <joaommpalmeiro@gmail.com>
2021-07-17 00:36:38 +0100

    Add 4, 5, and 6

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ A [Go](https://golang.org/) CLI to generate sequences of colored circles with em
   - [Go (Golang) Tutorial #1 - Introduction & Setup](https://youtu.be/etSN4X_fCnM).
   - [Go (Golang) Tutorial #2 - Your First Go File](https://youtu.be/RI9ngRqn9N4).
   - [Go (Golang) Tutorial #3 - Variables, Strings & Numbers](https://youtu.be/98tQPM3R3qU).
+  - [Go (Golang) Tutorial #4 - Printing & Formatting Strings](https://youtu.be/m1Uy0WQ2Xns).
+  - [Go (Golang) Tutorial #5 - Arrays & Slices](https://youtu.be/Arb-LjPg7FA).
+  - [Go (Golang) Tutorial #6 - The Standard Library](https://youtu.be/BoooHY57RXw).
 
 ## Notes
 
@@ -19,3 +22,7 @@ A [Go](https://golang.org/) CLI to generate sequences of colored circles with em
   - `go version` (Go 1.16).
   - `go run main.go`.
 - [Numeric types](https://golang.org/ref/spec#Numeric_types) and [types](https://pkg.go.dev/builtin#pkg-types).
+- [`fmt`](https://pkg.go.dev/fmt):
+  - `%f`: For floats.
+  - `%0.1f`: One decimal place.
+- [Standard library](https://pkg.go.dev/std) (built-in packages).
--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
 // Application entry point.
 func main() {
@@ -29,4 +33,44 @@ func main() {
 
 	// `%T`: Get the variable type.
 	fmt.Printf("Message: %T\n", message)
+
+	// Sprintf (save formatted strings).
+	var longMessage string = fmt.Sprintf("Message: %v", message)
+	fmt.Println(longMessage)
+
+	// Array (fixed length).
+	// var numbers [3]int = [3]int{1, 2, 3}
+	// var numbers = [3]int{1, 2, 3}
+	numbers := [3]int{1, 2, 3}
+	numbers[1] = 4
+
+	fmt.Println(numbers, len(numbers))
+
+	// Slices (closer to JavaScript arrays, for example).
+	var scores = []int{100, 200, 300}
+	scores[1] = 50
+
+	// It is not possible to do this with arrays.
+	scores = append(scores, 500)
+
+	fmt.Println(scores, len(scores))
+
+	// Slice ranges.
+	rangeNumbers := numbers[1:3]
+	fmt.Println(rangeNumbers)
+
+	// strings package.
+	fmt.Println(strings.Contains(message, "Hello"))
+	fmt.Println(strings.ReplaceAll(message, "Hello", "Hi"))
+	fmt.Println(strings.ToUpper(message))
+	fmt.Println(strings.Index(message, "ll")) // Position.
+	fmt.Println(strings.Split(message, " "))
+
+	// sort package.
+	// `sort.Strings()` for strings.
+	sort.Ints(scores) // In-place.
+	fmt.Println(scores)
+
+	// `sort.SearchStrings()` for strings.
+	fmt.Println(sort.SearchInts(scores, 50)) // Position.
 }
